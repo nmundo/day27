fa84278c8d90398867b4f64ea391f653aae19753
Nathan Mundo <nathanmundo@icloud.com>
2024-06-25 12:16:41 -0400

    added comments

--- script2.js
+++ script2.js
@@ -1,12 +1,16 @@
-const formModal = new bootstrap.Modal(document.getElementById('formModal'));
-const phoneTypes = ['Home', 'Cell', 'Work'];
+const formModal = new bootstrap.Modal(document.getElementById('formModal')); // Inititalize modal
+const phoneTypes = ['Home', 'Cell', 'Work']; // Array to map phones types
 
 const showContacts = () => {
   let contacts = JSON.parse(localStorage.getItem('contacts')) || [];
   const contactsList = document.getElementById('contactsList');
+
+  // Clear out contacts list
   while(contactsList.firstChild) {
     contactsList.removeChild(contactsList.firstChild);
   }
+
+  // Generate a new table row for each contact in the contacts array
   contacts.forEach(contact => {
     const row = document.createElement('tr');
     row.innerHTML = `
@@ -20,57 +24,81 @@ const showContacts = () => {
         <button class="btn btn-outline-primary" onclick="editContact('${contact.phoneNumber}')">Edit</button>
       </td>
     `;
+    // Add the newly created row to the contacts list
     contactsList.appendChild(row);
   })
 }
 
 const submitForm = () => {
-  const firstName = document.getElementById('firstName');
-  const lastName = document.getElementById('lastName');
-  const phoneType = document.getElementById('phoneType');
-  const phoneNumber = document.getElementById('phoneNumber');
-  const email = document.getElementById('email');
+  // Get values from form fields
+  const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
+  const phoneType = document.getElementById('phoneType').value;
+  const phoneNumber = document.getElementById('phoneNumber').value;
+  const email = document.getElementById('email').value;
   const mode = document.getElementById('mode').value;
+
+  // Create the contact object
   const newContact = {
-    firstName: firstName.value,
-    lastName: lastName.value,
-    phoneType: phoneType.value,
-    phoneNumber: phoneNumber.value,
-    email: email.value,
+    firstName,
+    lastName,
+    phoneType,
+    phoneNumber,
+    email,
   };
-  console.log(newContact);
+
+  // Get existing contacts array
   let contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+
+  // If form mode is edit then remove the contact we're editing from the contacts array
   if (mode === 'E') {
     contacts = contacts.filter(contact => contact.phoneNumber !== newContact.phoneNumber);
   }
+
+  // Set local storage to the existing contacts array + the new contact
   localStorage.setItem('contacts', JSON.stringify([...contacts, newContact]));
   formModal.hide();
   document.getElementById('contactForm').reset();
+
+  // Reload contacts display
   showContacts();
 }
 
 const deleteContact = (phoneNumber) => {
   let contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+
+  // Filter out contact we want to delete
   contacts = contacts.filter(contact => contact.phoneNumber !== phoneNumber);
   localStorage.setItem('contacts', JSON.stringify(contacts));
+
+  // Reload contacts display
   showContacts();
 }
 
 const editContact = (phoneNumber) => {
-  document.getElementById('mode').value = 'E';
+  document.getElementById('mode').value = 'E'; // Set form mode to edit
+
+  // Get contacts and find the one we want to edit
   const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
   const contact = contacts.find(contact => contact.phoneNumber === phoneNumber);
-  const firstName = document.getElementById('firstName');
-  const lastName = document.getElementById('lastName');
-  const phoneType = document.getElementById('phoneType');
+
+  // Get the form input fields
+  const firstNameField = document.getElementById('firstName');
+  const lastNameField = document.getElementById('lastName');
+  const phoneTypeField = document.getElementById('phoneType');
   const phoneNumberField = document.getElementById('phoneNumber');
-  const email = document.getElementById('email');
-  firstName.value = contact.firstName;
-  lastName.value = contact.lastName;
-  phoneType.value = contact.phoneType;
+  const emailField = document.getElementById('email');
+
+  // Pre fill form fields with existing contact values
+  firstNameField.value = contact.firstName;
+  lastNameField.value = contact.lastName;
+  phoneTypeField.value = contact.phoneType;
   phoneNumberField.value = contact.phoneNumber;
-  email.value = contact.email;
+  emailField.value = contact.email;
+
+  // Show the form
   formModal.show();
 }
 
+// Populate contacts table on initital page load
 showContacts();
